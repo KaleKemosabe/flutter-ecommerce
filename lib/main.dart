92763e2c8c969511ac202ccf729163e1a1923fc6
Kalle Kekomaki <kalle.kekomaki@neomore.fi>
2022-11-17 14:03:09 +0200

    Add navigation to product page

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: ProductsOverviewScreen(),
     );
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -10,7 +12,14 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl, fit: BoxFit.cover,),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title),
+          ),
+          );
+        },
+        child: Image.network(imageUrl, fit: BoxFit.cover,),
+      ),
       footer: GridTileBar(backgroundColor: Colors.black87, leading: IconButton(icon: Icon(Icons.favorite)), title: Text(title, textAlign: TextAlign.center,), trailing: IconButton(icon: Icon(Icons.shopping_cart,), onPressed: () {},)),
     );
   }
